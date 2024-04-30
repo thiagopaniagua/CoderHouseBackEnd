86c6b01a2b2af083be55eab163b7e9635e306187
thiagopaniagua <titi.paniaguacae@gmail.com>
2024-04-30 13:51:09 -0300

    Desafio3

--- Desafio3/src/ProductManager.js
+++ Desafio3/src/ProductManager.js
@@ -1,10 +1,10 @@
 import { promises as fs } from "fs";
 
-class ProductManager {
+export class ProductManager {
     constructor() {
         this.path = "./productos.json"; 
         this.products = [];
-        this.init(); // cargo los productos al inico
+        this.init(); // cargo los productos al inicio
     }
 
     static id = 0;
@@ -18,7 +18,7 @@ class ProductManager {
         }
     }
 
-    addProduct = async (title, description, price, thumbnail, code, stock) => {
+    async addProduct(title, description, price, thumbnail, code, stock) {
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             throw new Error('Todos los campos deben ser obligatorios');
         }
@@ -40,7 +40,7 @@ class ProductManager {
 
         this.products.push(newProduct); 
         await fs.writeFile(this.path, JSON.stringify(this.products, null, 4)); 
-    };
+    }
 
     async callProducts() {
         const data = await fs.readFile(this.path, "utf-8");
@@ -49,23 +49,23 @@ class ProductManager {
 
     async getProducts() {
         return this.callProducts(); 
-    };
+    }
 
     async getProductsById(id) {
         const products = await this.callProducts();
-        const product = products.find(product => product.id === id);
+        const product = products.find(product => product.id === parseInt(id));
         if (!product) {
             throw new Error(`El producto con el id ${id} no se ha encontrado o no existe`);
         }
         return product;
-    };
+    }
 
     async deleteProductsById(id) {
         const products = await this.callProducts();
         const filteredProducts = products.filter(product => product.id !== id);
         await fs.writeFile(this.path, JSON.stringify(filteredProducts, null, 4)); 
         console.log(`El producto con el id ${id} fue eliminado`);
-    };
+    }
 
     async updateProducts(id, updatedProperties) {
         const products = await this.callProducts();
@@ -79,33 +79,5 @@ class ProductManager {
         } else {
             throw new Error(`No se encontró ningún producto con el id ${id}`);
         }
-    };
-}
-
-const productos = new ProductManager();
-
-
-(async () => {
-    try {
-        await productos.addProduct("Monitor", "22 pulgadas", 120000, "imagenmonitor", "1", 7);
-        await productos.addProduct("teclado", "mecánico", 45000, "imagenteclado", "2", 14);
-        await productos.addProduct("mouse", "6000 dpi", 21000, "imagenmouse", "adaddn", 10);
-        await productos.addProduct("Auriculares", "inalámbricos", 85000, "imagenauriculares", "3", 5);
-        await productos.addProduct("Impresora", "multifunción", 155000, "imagenimpresora", "4", 3);
-        await productos.addProduct("DiscoDuro", "externo", 75000, "imagendiscoduro", "5", 8);
-        await productos.addProduct("CámaraSeguridad", "IP", 280000, "imagencamaraseguridad", "6", 2);
-        await productos.addProduct("Altavoces", "Bluetooth", 63000, "imagenaltavoces", "7", 6);
-        await productos.addProduct("MemoriaUSB", "alta velocidad", 40000, "imagenmemoriausb", "8", 10);
-        await productos.addProduct("Ratón", "ergonómico", 37000, "imagenraton", "9", 9);
-        await productos.addProduct("Teclado", "retroiluminado", 105000, "imagenteclado", "11", 4);
-        //await productos.addProduct("Monitor", "curvo", 220000, "imagenmonitor", "11", 3);
-       // await productos.addProduct("TabletaGráfica nashe", "profesional asd", 320000, "imagentableta", "12");
-
-        //console.log(await productos.getProducts());
-        // console.log(await productos.getProductsById(3));
-        // await productos.deleteProductsById(2);
-        //await productos.updateProducts(1, { title: "Monitor Samsung", code: "14" });
-    } catch (error) {
-        console.error("Error:", error.message);
     }
-})();
+}
---
+++ Desafio3/src/app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { ProductManager } from './ProductManager.js';
+
+const app = express();
+const port = 8080;
+const productos = new ProductManager();
+
+app.use(express.json());
+
+app.get('/products', async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || undefined;
+    const products = await productos.getProducts();
+    if (limit !== undefined) {
+      res.json(products.slice(0, limit));
+    } else {
+      res.json(products);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/products/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await productos.getProductsById(id);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
